94f793401515c41339832f607e5812f6de37ff90
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-11-15 18:03:10 +0700

    menambahkan tombol kembali

--- app/src/main/java/com/example/week7/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/week7/ui/view/TampilMahasiswaView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -47,6 +48,10 @@ fun TampilMahasiswaView(
             Judul = "NIM",
             isinya = mhs.nim
         )
+
+        Button(onClick = { navController.popBackStack() }) { // Navigate back to the previous screen
+            Text("Back")
+        }
     }
 }
 @Composable
